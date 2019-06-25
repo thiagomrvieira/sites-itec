4131da5d825df8c9142f420569bab2dd625231b6
Thiago Maciel <thiagomrvieira@gmail.com>
2019-06-25 13:01:27 -0300

    Adiciona logout e nome do usuário ao TopBar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -41,7 +41,7 @@
     </div>
     </li>
 
-    <!-- Nav Item - Alerts -->
+    {{-- <!-- Nav Item - Alerts -->
     <li class="nav-item dropdown no-arrow mx-1">
     <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fas fa-bell fa-fw"></i>
@@ -144,21 +144,21 @@
         </a>
         <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
     </div>
-    </li>
+    </li> --}}
 
     <div class="topbar-divider d-none d-sm-block"></div>
 
     <!-- Nav Item - User Information -->
     <li class="nav-item dropdown no-arrow">
     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
+        <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->name}}</span>
         <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
     </a>
     <!-- Dropdown - User Information -->
     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
         <a class="dropdown-item" href="#">
         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-        Profile
+        Perfil
         </a>
         {{-- <a class="dropdown-item" href="#">
         <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -168,11 +168,20 @@
         <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
         Activity Log
         </a> --}}
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-        Logout
+        
+
+        <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+            {{ __('Logout') }}
         </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+
+
     </div>
     </li>
 
