56d3486143537986f821de61d02aca1a076dbf14
Thiago Maciel <thiagomrvieira@gmail.com>
2019-06-26 22:17:22 -0300

    Ajustes no sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,21 +3,26 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-              <div class="sidebar-brand-icon rotate-n-15">
+              <!-- <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-laugh-wink"></i>
+              </div> -->
+              
+              <!-- <div class="sidebar-brand-text mx-3">Gov. Alagoas</div> -->
+              <div class="sidebar-brand-text mx-3">
+                <img src="{{asset('img/texto.png')}}" height="45">
               </div>
-              <div class="sidebar-brand-text mx-3">Gov. Alagoas</div>
+              
             </a>
       
             <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+            <!-- <hr class="sidebar-divider my-0"> -->
       
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
+            <!-- <li class="nav-item active">
               <a class="nav-link" href="index.html">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
-            </li>
+            </li> -->
       
             <!-- Divider -->
             <hr class="sidebar-divider">
