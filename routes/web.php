de4cb033cd2d5806cd9e75735e162d61f81009d1
Thiago Maciel <thiagomrvieira@gmail.com>
2019-06-28 13:43:53 -0300

    Redireciona de /home para /noticias caso esteja logado

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'Auth\LoginController@showLoginForm');
+
 
 Auth::routes();
 
